2e54f6676711e35eac61140a86edf94262a6e311
shahed762 <shahedjaman762@gmail.com>
2026-07-13 06:50:49 +0600

    fix(backend): CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,12 @@ dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // Allow all origins for dev, adjust in prod
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
